7f30f8f9f7bf4c6b10c0a70d0231b3cfa27cf674
Caelum van Ispelen <caelum12321@gmail.com>
2020-12-02 11:55:18 -0700

    Update naming conventions used in shaders

--- assets/shader/chunk/fragment.glsl
+++ assets/shader/chunk/fragment.glsl
@@ -1,37 +1,37 @@
 #version 440
 
-layout (location = 0) in vec3 v_texcoord;
-layout (location = 1) in vec3 v_view_pos;
-layout (location = 2) in vec3 v_world_pos;
-layout (location = 3) in vec3 v_normal;
+layout (location = 0) in vec3 iTexCoord;
+layout (location = 1) in vec3 iViewPos;
+layout (location = 2) in vec3 iWorldPos;
+layout (location = 3) in vec3 iNormal;
 
-layout (location = 0) out vec4 f_color;
+layout (location = 0) out vec4 oColor;
 
-layout (set = 0, binding = 1) uniform texture2DArray u_block_textures;
-layout (set = 0, binding = 2) uniform sampler u_block_sampler;
+layout (set = 0, binding = 1) uniform texture2DArray uBlockTextures;
+layout (set = 0, binding = 2) uniform sampler uBlockSampler;
 
-const vec4 fog_color = vec4(0.6, 0.7, 0.8, 1.0);
-const float fog_density = 0.005;
+const vec4 fogColor = vec4(0.6, 0.7, 0.8, 1.0);
+const float fogDensity = 0.005;
 
-const vec3 light_dir1 = vec3(1.0, 1.0, 0.3);
-const vec3 light_dir2 = vec3(-0.4, -0.7, -0.8);
-const vec3 light_color = vec3(1.0, 0.8, 0.5);
+const vec3 lightDir1 = vec3(1.0, 1.0, 0.3);
+const vec3 lightDir2 = vec3(-0.4, -0.7, -0.8);
+const vec3 lightColor = vec3(1.0, 0.8, 0.5);
 
 void main() {
     // Shading
     float ambient = 0.3;
-    vec3 normal = normalize(v_normal);
-    float diff = max(dot(normal, light_dir1), 0.0) + max(dot(normal, light_dir2), 0.0) * 0.4;
-    vec4 shaded = vec4((ambient + diff) * light_color, 1.0);
+    vec3 normal = normalize(iNormal);
+    float diff = max(dot(normal, lightDir1), 0.0) + max(dot(normal, lightDir2), 0.0) * 0.4;
+    vec4 shaded = vec4((ambient + diff) * lightColor, 1.0);
 
     // Compute fog
-    float fog_depth = length(v_view_pos);
+    float fogDepth = length(iViewPos);
     #define LOG2 1.442695
-    float fog_amount = 1. - exp2(-fog_density * fog_density * fog_depth * fog_depth * LOG2);
+    float fogAmount = 1. - exp2(-fogDensity * fogDensity * fogDepth * fogDepth * LOG2);
 
-    vec4 col = shaded * texture(sampler2DArray(u_block_textures, u_block_sampler), v_texcoord);
+    vec4 col = shaded * texture(sampler2DArray(uBlockTextures, uBlockSampler), iTexCoord);
 
-    col = mix(col, fog_color, fog_amount);
+    col = mix(col, fogColor, fogAmount);
 
-    f_color = col;
+    oColor = col;
 }
--- assets/shader/chunk/vertex.glsl
+++ assets/shader/chunk/vertex.glsl
@@ -1,31 +1,30 @@
 #version 440
 
-layout (location = 0) in vec3 a_pos;
-layout (location = 1) in vec3 a_texcoord;
-layout (location = 2) in vec3 a_normal;
+layout (location = 0) in vec3 iPos;
+layout (location = 1) in vec3 iTexCoord;
+layout (location = 2) in vec3 iNormal;
 
-layout (location = 0) out vec3 v_texcoord;
-layout (location = 1) out vec3 v_view_pos;
-layout (location = 2) out vec3 v_world_pos;
-layout (location = 3) out vec3 v_normal;
+layout (location = 0) out vec3 oTexCoord;
+layout (location = 1) out vec3 oViewPos;
+layout (location = 2) out vec3 oWorldPos;
+layout (location = 3) out vec3 oNormal;
 
-layout (set = 0, binding = 0) uniform Uniforms {
-    mat4 u_transform;
+layout (set = 0, binding = 0) uniform Locals {
+    mat4 uTransform;
 };
-layout (push_constant) uniform PushConstants {
-    mat4 u_view;
-    mat4 u_perspective;
+layout (push_constant) uniform Globals {
+    mat4 uView;
+    mat4 uPerspective;
 };
 
 void main() {
-    v_texcoord = a_texcoord;
+    oTexCoord = iTexCoord;
+    oNormal = iNormal;
 
-    v_view_pos = (u_view * u_transform * vec4(a_pos, 1.0)).xyz;
+    oWorldPos = (uTransform * vec4(iPos, 1.0)).xyz;
 
-    v_world_pos = (u_transform * vec4(a_pos, 1.0)).xyz;
+    oViewPos = (uView * vec4(oWorldPos, 1.0)).xyz;
 
-    vec4 camera_pos = u_perspective * u_view * u_transform * vec4(a_pos, 1.0);
-    gl_Position = camera_pos;
-
-    v_normal = a_normal;
+    vec4 cameraPos = uPerspective * vec4(oViewPos, 1.0);
+    gl_Position = cameraPos;
 }
